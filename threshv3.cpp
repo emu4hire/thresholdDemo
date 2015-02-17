46311868acec2ad01afd357a3df1396e2eb0b836
emu4hire <emu4hire@gmail.com>
2015-02-17 11:21:14 -0500

    frame saving and code organization/cleanup

--- threshv3.cpp
+++ threshv3.cpp
@@ -5,79 +5,137 @@
 using namespace cv;
 using namespace std;
 
- int main( int argc, char** argv )
- {
-   VideoCapture cap;
+Mat operation(Mat, Scalar, Scalar);
+void saveframe(Mat, Mat, int);
 
-   if(argc >1)
-	cap.open(argv[1]); //read in video from command line arguments
-   else
-   	cap.open(0); //capture the video from web cam
+int main( int argc, char** argv ){
 
-    if ( !cap.isOpened() )  // if not success, exit program
-    {
-         cout << "Cannot open the web cam" << endl;
-         return -1;
-    }
+	VideoCapture cap;
 
-    namedWindow("Control", CV_WINDOW_AUTOSIZE); 
+	if(argc >1)
+		cap.open(argv[1]); //read in video from command line arguments
+	else
+		cap.open(0); //capture the video from web cam
 
-  int iLowH = 0;
- int iHighH = 179;
 
-  int iLowS = 0; 
- int iHighS = 255;
+	if ( !cap.isOpened() )  // if not success, exit program
+	{
+		cout << "Cannot open the web cam" << endl;
+		return -1;
+	}
 
-  int iLowV = 0;
- int iHighV = 255;
+	namedWindow("Control", CV_WINDOW_AUTOSIZE); 
+		
 
-  //Create trackbars in "Control" window
- cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
- cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+	int iLowH = 0;
+	int iHighH = 179;
 
-  cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
- cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+	int iLowS = 0; 
+	int iHighS = 255;
 
-  cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
- cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+	int iLowV = 0;
+	int iHighV = 255;
 
-    while (true)
-    {
-        Mat imgOriginal;
+	//Create trackbars in "Control" window
+	cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	cvCreateTrackbar("HighH", "Control", &iHighH, 179);
 
-        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+	cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	cvCreateTrackbar("HighS", "Control", &iHighS, 255);
 
-         if (!bSuccess) //if not success, break loop
-        {
-             cout << "Cannot read a frame from video stream" << endl;
-             break;
-        }
-   Mat imgHSV;
+	cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+	cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+	
+	int frameNum= 0;
 
-   cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert from BGR to HSV
- 
-  Mat imgThresholded;
+	while (true)
+	{
+		Mat imgOriginal;
+		Scalar lower=  Scalar(iLowH, iLowS, iLowV);
+		Scalar upper=  Scalar(iHighH, iHighS, iHighV);
+		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-  inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
-      
-  //morphological opening
-  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+	        }
 
-   //morphological closing
-  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+		//Preform thrsholding operation
+		Mat imgThresholded= operation(imgOriginal, lower, upper);
 
-   imshow("Thresholded Image", imgThresholded); //show the thresholded image
-  imshow("Original", imgOriginal); //show the original image
+		imshow("Thresholded Image", imgThresholded); //show the thresholded image
+		imshow("Original", imgOriginal); //show the original image
 
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-       {
-            cout << "Video Steam Ended" << endl;
-            break; 
-       }
-    }
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		{
+			cout << "Video Steam Ended" << endl;
+			break; 
+		}
 
-   return 0;
+		if( waitKey(30)== 13){ //Wait for enter key press for 30ms.  If 'enter' is pressed, save the original and thresholded frames
+			saveframe(imgOriginal, imgThresholded, frameNum);
+		}
 
+		frameNum++;
+	}
+
+	return 0;
+
+}
+
+/*
+Perform thresholding operations
+*/
+Mat operation(Mat imgOriginal, Scalar lower, Scalar upper){
+
+	//Convert image from BGR to HSV.
+	Mat imgHSV;
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert from BGR to HSV
+
+	//Threshold HSV images
+        Mat imgThresholded;
+        inRange(imgHSV, lower, upper, imgThresholded);
+
+        //morphological opening
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+        //morphological closing
+        dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+	return imgThresholded;
+}
+
+/*
+Save the orignal and thresholded frames after generating a unique filename.
+*/
+void saveframe(Mat imgOriginal, Mat imgThresholded, int frameNum){
+
+	String path= "/home/michael/Desktop/";
+        String ext= ".png";
+
+        String number;
+        ostringstream convert;
+        convert << frameNum;
+        number= convert.str();
+
+        String original= path+"original";
+        String threshed= path+"thresholed";
+        original=original+number;
+        threshed=threshed+number;
+        original=original+ext;
+        threshed=threshed+ext;
+
+        vector<int> params;
+        params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+        params.push_back(9);
+
+
+        if(!imwrite(original, imgOriginal, params))
+        	cerr<<"Failed to write original"<<endl;
+
+	if(!imwrite(threshed, imgThresholded, params))
+        	cerr<<"Failed to write thresholded"<<endl;
 }
